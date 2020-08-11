ab7dcd8e7d8e2cb55eeb84bba2f514688c97c90f
yunchong qu <quyunchong@autowise.ai>
2020-08-11 11:31:54 +0800

    LC每日一题

---
+++ leetcode_train/LC130.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * 本题给定的矩阵中有三种元素：
+
+    字母 X；
+    被字母 X 包围的字母 O；
+    没有被字母 X 包围的字母 O。
+
+本题要求将所有被字母 X 包围的字母 O都变为字母 X ，但很难判断哪些 O 是被包围的，哪些 O 不是被包围的。
+
+注意到题目解释中提到：任何边界上的 O 都不会被填充为 X。 我们可以想到，所有的不被包围的 O 都直接或间接与边界上的 O 相连。我们可以利用这个性质判断 O 是否在边界上，具体地说：
+
+    对于每一个边界上的 O，我们以它为起点，标记所有与它直接或间接相连的字母 O；
+    最后我们遍历这个矩阵，对于每一个字母：
+        如果该字母被标记过，则该字母为没有被字母 X 包围的字母 O，我们将其还原为字母 O；
+        如果该字母没有被标记过，则该字母为被字母 X 包围的字母 O，我们将其修改为字母 X。
+
+**/
+
+//dfs
+class Solution {
+public:
+    int n, m;
+
+    void dfs(vector<vector<char>>& board, int x, int y) {
+        if (x < 0 || x >= n || y < 0 || y >= m || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x + 1, y);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x, y - 1);
+    }
+
+    void solve(vector<vector<char>>& board) {
+        n = board.size();
+        if (n == 0) {
+            return;
+        }
+        m = board[0].size();
+        for (int i = 0; i < n; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, m - 1);
+        }
+        for (int i = 1; i < m - 1; i++) {
+            dfs(board, 0, i);
+            dfs(board, n - 1, i);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
+
+//bfs
+class Solution1 {
+public:
+    const int dx[4] = {1, -1, 0, 0};
+    const int dy[4] = {0, 0, 1, -1};
+
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        if (n == 0) {
+            return;
+        }
+        int m = board[0].size();
+        queue<pair<int, int>> que;
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
+                que.emplace(i, 0);
+            }
+            if (board[i][m - 1] == 'O') {
+                que.emplace(i, m - 1);
+            }
+        }
+        for (int i = 1; i < m - 1; i++) {
+            if (board[0][i] == 'O') {
+                que.emplace(0, i);   
+            }
+            if (board[n - 1][i] == 'O') {
+                que.emplace(n - 1, i);
+            }
+        }
+        while (!que.empty()) {
+            int x = que.front().first, y = que.front().second;
+            que.pop();
+            board[x][y] = 'A';
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx < 0 || my < 0 || mx >= n || my >= m || board[mx][my] != 'O') {
+                    continue;
+                }
+                que.emplace(mx, my);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'A') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
+
+
+int main(){
+    cout<<"This is a leetcode train file.";
+    return 0;
+}
