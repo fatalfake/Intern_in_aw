57f0949bb4067dd8ff7897507c924079074cecb8
yunchong qu <quyunchong@autowise.ai>
2020-08-14 17:55:20 +0800

    回家在做

---
+++ leetcode_train/LCcjsf02.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+#define INT_MAX 2147483647
+
+using namespace std;
+
+// 买卖股票的最佳时机II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
+
+int main()
+{
+    vector<int> b;
+    b = vector<int>();
+    cout << "This is a leetcode train file."<<endl;
+    for(int k = 1; k < 50; k++){
+        cout<<"K: "<<k<<"    "<<"K&1:  "<<(k&1)<<endl;
+    }
+    return 0;
+}
