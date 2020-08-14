2b914f1efc94e6d135ba3778232e425b3bf723c7
yunchong qu <quyunchong@autowise.ai>
2020-08-14 16:47:36 +0800

    fix port conflict

--- regression_parallelly.py
+++ regression_parallelly.py
@@ -716,11 +716,25 @@ def run_single_case(run_id, case_dir, port, vehicle, version, record=False):
     SIMU_LOG_FILE = os.path.join(case_dir, SIMU_LOG_FILE)
     # logf = open(SIMU_LOG_FILE, 'w')            
     
-    check_record_cmd = "netstat -lntp | grep record | grep 127.0.0.1:%s" %port
-    check_record_process = subprocess.Popen(check_record_cmd, stdout=subprocess.PIPE, shell=True)
-    check_record_info = check_record_process.stdout.read()
-    if len(check_record_info)!=0:            
-        port = str(int(port) + 1)
+    check_port_cmd = "netstat -lntp | grep 127.0.0.1:"
+    check_port_process = subprocess.Popen(check_port_cmd, stdout=subprocess.PIPE, shell=True)
+    check_port_info = check_port_process.stdout.read()
+    check_port_info_lines = check_port_info.split('\n')
+    check_port_info_lines.remove(check_port_info_lines[-1])
+
+    forbid_port_list = []
+
+    for i in check_port_info_lines:
+        j = re.split(r"[ ]+", i)
+        l = j[3].split(':')[1]
+        forbid_port_list.append(l)
+
+    forbid_port_list.sort()
+    if port in forbid_port_list:
+        while True:
+            port = str(int(port) + 1)
+            if port not in forbid_port_list:
+                break
     
     if record == False:
         command = "python new_playcase.py %s --exit --port=%s --version=%s --run_id=%d; sleep 3" % (
--- sh_py_exit_test/record_port_check.py
+++ sh_py_exit_test/record_port_check.py
@@ -1,8 +1,8 @@
 import subprocess
+import re
 
-str1 = ".0.0:"
 
-cmd = "netstat -lntp | grep rviz | grep 0.0%s" %str1
+cmd = "netstat -lntp | grep 127.0.0.1"
 
 s = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 
@@ -10,3 +10,32 @@ p = s.stdout.read()
 
 print len(p)
 print p
+
+k = p.split('\n')
+k.remove(k[-1])
+
+forbid_port_list = []
+
+for i in k:
+    # print i
+    j = re.split(r"[ ]+", i)
+    # print j
+    l = j[3].split(':')[1]
+    # print l
+    forbid_port_list.append(l)
+
+
+forbid_port_list.append('100')
+forbid_port_list.append('200')
+
+forbid_port_list.sort()
+print int(forbid_port_list[0])
+
+ppp = "100"
+# ppp = 100
+if ppp in forbid_port_list:
+    print "YES"
+
+if ppp not in forbid_port_list:
+    print "NO"
+
