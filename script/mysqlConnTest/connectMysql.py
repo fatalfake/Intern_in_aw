4c7b5ce08b50efa6d94e59d62170dd5fac713692
yunchong qu <quyunchong@autowise.ai>
2020-10-22 20:03:06 +0800

    python连接mysql

--- script/mysqlConnTest/connectMysql.py
+++ script/mysqlConnTest/connectMysql.py
@@ -1,7 +1,8 @@
 # -*-coding:utf-8-*-
-import pymysql
+import sys
 import datetime
 import calendar
+import pymysql
 
 
 def get_start_date():
@@ -19,6 +20,13 @@ def get_start_date():
 
 
 # 链接
+# conn = pymysql.connect(host='szjjh-webserver.szjjh.autowise.ai',
+#                        user='aw_monitor',
+#                        password='123Qwe',
+#                        database='aw_monitor_system',
+#                        charset='utf8',
+#                        cursorclass=pymysql.cursors.DictCursor)
+
 conn = pymysql.connect(host='szjjh-webserver.szjjh.autowise.ai',
                        user='aw_monitor',
                        password='123Qwe',
@@ -29,23 +37,35 @@ conn = pymysql.connect(host='szjjh-webserver.szjjh.autowise.ai',
 # 执行完毕返回的结果集默认以元组显示
 # cursorclass=pymysql.cursors.DictCursor 配置结果集为字典形式
 
-try:
-    # 游标
-    with conn.cursor() as cursor:
-        # 执行sql语句
-        sql = "select year(begin_timestamp),  week(begin_timestamp,1),  count(distinct(vehicle_id)) as beijing_vehicles from task_execution  where begin_timestamp > \"%s\" and park_id in ('beijing_huanbao_park','beijing_haidian_jiaxiao') group by year(begin_timestamp),week(begin_timestamp,1) order by year(begin_timestamp), week(begin_timestamp, 1)" % get_start_date()
-        print(sql)
-        # 执行sql语句，返回sql查询成功的记录数目,我只在表中插入一条记录，查询成功最多所以也就一条记录数
-        cursor.execute(sql)
-        # 连接完数据库并不会自动提交，所以需要手动 commit 你的改动
-        # conn.commit()
-        res = cursor.fetchall()
-        print(res)
-        cursor.close()
+def test_func():
+    try:
+        # 游标
+        with conn.cursor() as cursor:
+            # 执行sql语句
+            sql = "select year(begin_timestamp),  week(begin_timestamp,1),  count(distinct(vehicle_id)) as beijing_vehicles from task_execution  where begin_timestamp > \"%s\" and park_id in ('beijing_huanbao_park','beijing_haidian_jiaxiao') group by year(begin_timestamp),week(begin_timestamp,1) order by year(begin_timestamp), week(begin_timestamp, 1)" % get_start_date()
+            print(sql)
+            # 执行sql语句，返回sql查询成功的记录数目,我只在表中插入一条记录，查询成功最多所以也就一条记录数
+            cursor.execute(sql)
+            # 连接完数据库并不会自动提交，所以需要手动 commit 你的改动
+            # conn.commit()
+            res = cursor.fetchall()
+            # print(res)
+            cursor.close()
 
-except pymysql.Error as e:
-    print ("Mysql Error")
-    print (e)
+    except pymysql.Error as e:
+        print ("Mysql Error")
+        print (e)
 
-finally:
-    conn.close()
+    finally:
+        conn.close()
+        return res
+
+    
+res = test_func()
+sys.stdout.write("Type of the whole result is: ")
+#使用stdout可以不换行输出
+# print ("Type of the whole result is: ") 
+print (type(res))
+sys.stdout.write("Type of the a single result is: ")
+# print ("Type of the a single result is: ") 
+print (type(res[0]))
